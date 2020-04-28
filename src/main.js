429feb83d5754470d6f131a29c3f833480bbcc8b
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-04-28 16:50:37 +0300

    Add Vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css'
 import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vuetify from 'vuetify'
 import App from './App'
 require('./assets/app.css')
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
