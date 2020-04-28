855f2a8e39fd1db7c28213eb14d1675c340ab387
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-04-28 16:44:37 +0300

    Uninstall bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
