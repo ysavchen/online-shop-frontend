6021cee9cfe3c1540369c58f77f7d7c11e913817
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-05-22 10:07:11 +0300

    Fix misprint

--- src/mixins/formRules.js
+++ src/mixins/formRules.js
@@ -3,7 +3,7 @@ export default {
         rules: {
             required: value => !!value || 'Required.',
             minChars: v => (v && v.length >= 8) || 'Min 8 characters',
-            emailValitiy: v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            emailValidity: v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
         }
     }),
 }
