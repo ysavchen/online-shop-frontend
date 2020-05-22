5353bf45a8ad89d9c2faf7df1f6a2c2014c68d71
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-05-23 01:39:56 +0300

    Protect authenticated path

--- src/router/index.js
+++ src/router/index.js
@@ -5,39 +5,26 @@ import Checkout from '@/views/Checkout.vue'
 import BookDetails from '@/views/BookDetails.vue'
 import Order from '@/views/Order.vue'
 import UserOrders from '@/views/UserOrders.vue'
+import security from '@/store/modules/security'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', name: 'books', component: Books },
+  { path: '/books/:id', name: 'bookDetails', component: BookDetails },
+  { path: '/checkout', name: 'checkout', component: Checkout },
+  { path: '/order', name: 'order', component: Order },
   {
-    path: '/',
-    name: 'books',
-    component: Books
+    path: '/users/:id/orders', name: 'userOrders', component: UserOrders,
+    beforeEnter(to, from, next) {
+      if (security.state.token) {
+        next()
+      } else {
+        next('/')
+      }
+    }
   },
-  {
-    path: '/books/:id',
-    name: 'bookDetails',
-    component: BookDetails
-  },
-  {
-    path: '/checkout',
-    name: 'checkout',
-    component: Checkout
-  },
-  {
-    path: '/order',
-    name: 'order',
-    component: Order
-  },
-  {
-    path: '/users/:id/orders',
-    name: 'userOrders',
-    component: UserOrders
-  },
-  {
-    path: '*',
-    redirect: '/'
-  }
+  { path: '*', redirect: '/' }
 ]
 
 export default new VueRouter({
